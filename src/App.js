4bc66a79da421e64d8fd0c5d0a931d55524afb76
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2023-10-09 20:03:54 +0300

    Initialize and make first tests

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import UserForm from './components/UserForm';
+import UserList from './components/UserList';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  const onUserAdd = (user) => {
+    setUsers([...users, user]);
+  };
+
+  return (
+    <div>
+      <UserForm onUserAdd={onUserAdd} />
+      <hr />
+      <UserList users={users} />
+    </div>
+  );
+}
+
+export default App;
