3d58b2f6079cee7325cbf19d1eb1dd5504c359b4
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2023-10-12 19:08:24 +0300

    Update comment

--- src/tests/Posts.test.js
+++ src/tests/Posts.test.js
@@ -2,22 +2,21 @@ import { render, screen } from '@testing-library/react';
 import axios from 'axios';
 import Posts from '../components/Posts';
 
-// in case if we don't use msw
+// in case we don't use msw
 jest.mock('axios');
 
 const mockСomments = [
   {
     userId: 1,
     id: 1,
-    title:
-      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    title: 'Title 1',
+    body: 'Body 1',
   },
   {
     userId: 1,
     id: 2,
-    title: 'qui est esse',
-    body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+    title: 'Title 2',
+    body: 'Body 2',
   },
 ];
 
